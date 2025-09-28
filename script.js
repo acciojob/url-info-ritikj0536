0f13c36e5ad08944caa49e4b8adf853b7fec3b07
ritikj0536 <124515293+ritikj0536@users.noreply.github.com>
2025-09-28 18:44:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// Get current URL
+let currentUrl = window.location.href;
+
+// Get length of URL
+let urlLength = currentUrl.length;
+
+// Show result
+alert("The length of the URL is: " + urlLength);
